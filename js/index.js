9e71704d7b2f17d2f2f8f502930885e67ba1db29
V.Matteus <80211527+VicMatteus@users.noreply.github.com>
2022-04-18 16:16:41 -0300

    Add files via upload
    Exercício respondido. Ao clicar no botão, é exibida na tela, o retorno da api

--- js/index.js
+++ js/index.js
@@ -1,14 +1,38 @@
 // Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
-fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        //manipulamos a resposta
-        console.log(data)
-    });
+document.getElementById('random').addEventListener('click', function()
+{
+    fetch('https://randomuser.me/api/')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            //manipulamos a resposta
+            console.log(data.results[0]);
+                renderizarDadosUsuario(data.results[0]);
+        });
+});
 
 function renderizarDadosUsuario(dados) {
+    let container = document.querySelector('div.card');
+    container.innerHTML = '';
+
+    let infoNome = dados.name;
+    let nomeCompleto = infoNome.title + " " + infoNome.first + " " + infoNome.last;
+    let picture = dados.picture.large;
+    let email = dados.email;
+
+    let nameNode = document.createElement('h2').appendChild(document.createTextNode(nomeCompleto));
+    
+    let img = document.createElement('img');
+    img.setAttribute("src", picture);
+    
+    let mailText = document.createTextNode(email);
+    
+    container.classList.add('card')
+    container.appendChild(nameNode);
+    container.appendChild(img);
+    container.appendChild(mailText);
+    document.querySelector('div.card').appendChild(container);
     /* -------------------------------- Tarefa 1 -------------------------------- */
     // Aqui devem desenvolver uma função que seja exibida na tela:
     // a foto, o nome completo do usuário e o e-mail.
